48b28da09704efaad03fdb6af7969441631dbe14
Pablo Rodrigues Quadrado <pabloquadrado27@gmail.com>
2020-07-28 22:03:50 -0300

    Adiciona babel e webpack ao projeto

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Hello World
-                </p>
-
-            </header>
-        </div>
+        <h1>Hello World!</h1>
     );
 }
 
